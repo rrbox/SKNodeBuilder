5dd24f50ede3368009e622c466d5ebcd293552b1
rrbox <87851278+rrbox@users.noreply.github.com>
2022-04-29 15:13:35 +0900

    Add : ShapeBuilder test

--- Tests/SKNodeBuilderTests/SKNodeBuilderTests.swift
+++ Tests/SKNodeBuilderTests/SKNodeBuilderTests.swift
@@ -80,4 +80,32 @@ final class SKNodeBuilderTests: XCTestCase {
 #endif
     }
     
+    func testShapeBuilder() throws {
+        
+        let node = Builder<SKShapeNode>()
+            .path(CGPath(rect: CGRect(x: 0, y: 0, width: 32, height: 32), transform: nil))
+            .fillColor(.red)
+            .lineWidth(1)
+            .strokeColor(.green)
+            .glowWidth(2)
+            .lineCap(.round)
+            .lineJoin(.round)
+            .miterLimit(3)
+            .isAntialiased(false)
+            .blendMode(.add)
+            .node
+        
+        XCTAssertEqual(node.path, CGPath(rect: CGRect(x: 0, y: 0, width: 32, height: 32), transform: nil))
+        XCTAssertEqual(node.fillColor, .red)
+        XCTAssertEqual(node.lineWidth, 1)
+        XCTAssertEqual(node.strokeColor, .green)
+        XCTAssertEqual(node.glowWidth, 2)
+        XCTAssertEqual(node.lineCap, .round)
+        XCTAssertEqual(node.lineJoin, .round)
+        XCTAssertEqual(node.miterLimit, 3)
+        XCTAssertEqual(node.isAntialiased, false)
+        XCTAssertEqual(node.blendMode, .add)
+        
+    }
+    
 }
