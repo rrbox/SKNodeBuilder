9ebe12efcb9abb9aa4554f2b0780d2589eca1a6f
rrbox <87851278+rrbox@users.noreply.github.com>
2022-09-06 22:33:14 +0900

    [remove] Property wrapper Builder の実装方法の変更に伴い, property wrapper が機能しなくなったため, 一時的に削除とした.

--- Sources/SKNodeBuilder/Foundation/PropertyWrapper.swift
+++ Sources/SKNodeBuilder/Foundation/PropertyWrapper.swift
@@ -4,15 +4,15 @@
 //
 //  Created by rrbox on 2021/11/20.
 //
-import SpriteKit
+//import SpriteKit
 
-@propertyWrapper
-public struct NodeBuilder<Body: SKNode> {
-    public var wrappedValue: Body
-    public init(wrappedValue: Body) {
-        self.wrappedValue = wrappedValue
-    }
-    public var projectedValue: UnownedNodeBuilder<Body> {
-        UnownedNodeBuilder(self.wrappedValue)
-    }
-}
+//@propertyWrapper
+//public struct NodeBuilder<Body: SKNode> {
+//    public var wrappedValue: Body
+//    public init(wrappedValue: Body) {
+//        self.wrappedValue = wrappedValue
+//    }
+//    public var projectedValue: UnownedNodeBuilder<Body> {
+//        UnownedNodeBuilder(self.wrappedValue)
+//    }
+//}
