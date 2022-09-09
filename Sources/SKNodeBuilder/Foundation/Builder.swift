e2310edf0bdc4f966b6df2b4a804a325ca632b38
rrbox <87851278+rrbox@users.noreply.github.com>
2022-09-09 22:15:20 +0900

    [add] Builder.modifier メソッド イニシャライザストラテジは保留して, モディファイアフローの構築のみ実装した

--- Sources/SKNodeBuilder/Foundation/Builder.swift
+++ Sources/SKNodeBuilder/Foundation/Builder.swift
@@ -8,11 +8,17 @@
 import SpriteKit
 
 public struct Builder<T: ProcessorProtocol> {
+    public typealias Next<Mod: Modifier> = Builder<Link<T, Mod>> where Mod.Node == T.Node
+    
     let processor: T
     
     public func node() -> T.Node {
         let result = T.Node()
         self.processor.mod(node: result)
         return result
+    }
+    
+    public func modifier<Mod: Modifier>(_ mod: Mod) -> Next<Mod> {
+        Next<Mod>.init(processor: .init(previous: self.processor, modData: mod))
     }
 }
