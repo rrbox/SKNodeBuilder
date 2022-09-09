0ce170cc68fe262714035100bf54755c9d3c9cc2
rrbox <87851278+rrbox@users.noreply.github.com>
2022-09-09 22:03:35 +0900

    [add] Builder 新しい定義の Builder を作成した. イニシャライザのストラテジとプロセッサを保持させる予定.

--- Sources/SKNodeBuilder/Foundation/Builder.swift
+++ Sources/SKNodeBuilder/Foundation/Builder.swift
@@ -5,4 +5,14 @@
 //  Created by rrbox on 2022/09/09.
 //
 
-import Foundation
+import SpriteKit
+
+public struct Builder<T: ProcessorProtocol> {
+    let processor: T
+    
+    public func node() -> T.Node {
+        let result = T.Node()
+        self.processor.mod(node: result)
+        return result
+    }
+}
