a80a22129f3e606bb14809b6165de373417e0972
rrbox <87851278+rrbox@users.noreply.github.com>
2022-11-03 02:31:39 +0900

    [update] SKSpriteNode 向けの processor の機能を SKSpriteNode のサブクラスに対しても利用できるようにした.

--- Sources/SKNodeBuilder/Sprite/SpriteProcessor.swift
+++ Sources/SKNodeBuilder/Sprite/SpriteProcessor.swift
@@ -11,107 +11,107 @@ public typealias SpriteProcessor<Node: SKSpriteNode> = Processor<Node>
 
 /// Name space of SKSpriteNode builder's modifiers.
 public enum SpriteModifiers {
-    public struct Texture: Modifier {
+    public struct Texture<Node: SKSpriteNode>: Modifier {
         var body: SKTexture
         
-        public func mod(node: SKSpriteNode) {
+        public func mod(node: Node) {
             node.texture = self.body
         }
     }
     
-    public struct Size: Modifier {
+    public struct Size<Node: SKSpriteNode>: Modifier {
         var body: CGSize
         
-        public func mod(node: SKSpriteNode) {
+        public func mod(node: Node) {
             node.size = self.body
         }
     }
     
-    public struct AnchorPoint: Modifier {
+    public struct AnchorPoint<Node: SKSpriteNode>: Modifier {
         var body: CGPoint
         
-        public func mod(node: SKSpriteNode) {
+        public func mod(node: Node) {
             node.anchorPoint = self.body
         }
     }
     
-    public struct CenterRect: Modifier {
+    public struct CenterRect<Node: SKSpriteNode>: Modifier {
         var body: CGRect
         
-        public func mod(node: SKSpriteNode) {
+        public func mod(node: Node) {
             node.centerRect = self.body
         }
     }
     
-    public struct Color: Modifier {
+    public struct Color<Node: SKSpriteNode>: Modifier {
         var body: SKColor
         
-        public func mod(node: SKSpriteNode) {
+        public func mod(node: Node) {
             node.color = self.body
         }
     }
     
-    public struct ColorBlendFactor: Modifier {
+    public struct ColorBlendFactor<Node: SKSpriteNode>: Modifier {
         var body: CGFloat
         
-        public func mod(node: SKSpriteNode) {
+        public func mod(node: Node) {
             node.colorBlendFactor = self.body
         }
     }
 
-    public struct BlendMode: Modifier {
+    public struct BlendMode<Node: SKSpriteNode>: Modifier {
         var body: SKBlendMode
         
-        public func mod(node: SKSpriteNode) {
+        public func mod(node: Node) {
             node.blendMode = self.body
         }
     }
 
-    public struct LightingBitMask: Modifier {
+    public struct LightingBitMask<Node: SKSpriteNode>: Modifier {
         var body: UInt32
         
-        public func mod(node: SKSpriteNode) {
+        public func mod(node: Node) {
             node.lightingBitMask = self.body
         }
     }
 
-    public struct ShadowedBitMask: Modifier {
+    public struct ShadowedBitMask<Node: SKSpriteNode>: Modifier {
         var body: UInt32
         
-        public func mod(node: SKSpriteNode) {
+        public func mod(node: Node) {
             node.shadowedBitMask = self.body
         }
     }
     
-    public struct ShadowCastBitMask: Modifier {
+    public struct ShadowCastBitMask<Node: SKSpriteNode>: Modifier {
         var body: UInt32
         
-        public func mod(node: SKSpriteNode) {
+        public func mod(node: Node) {
             node.shadowCastBitMask = self.body
         }
     }
 
-    public struct NormalTexture: Modifier {
+    public struct NormalTexture<Node: SKSpriteNode>: Modifier {
         var body: SKTexture?
         
-        public func mod(node: SKSpriteNode) {
+        public func mod(node: Node) {
             node.normalTexture = self.body
         }
     }
 
-    public struct Shader: Modifier {
+    public struct Shader<Node: SKSpriteNode>: Modifier {
         var body: SKShader?
         
-        public func mod(node: SKSpriteNode) {
+        public func mod(node: Node) {
             node.shader = self.body
         }
     }
     
-    public struct SetValue: Modifier {
+    public struct SetValue<Node: SKSpriteNode>: Modifier {
         var value: SKAttributeValue
         var key: String
         
-        public func mod(node: SKSpriteNode) {
+        public func mod(node: Node) {
             node.setValue(self.value, forAttribute: self.key)
         }
     }
@@ -120,56 +120,56 @@ public enum SpriteModifiers {
     
 }
 
-public extension ProcessorProtocol where Node == SKSpriteNode {
-    @discardableResult func texture(_ value: SKTexture) -> Next<SpriteModifiers.Texture> {
+public extension ProcessorProtocol where Node: SKSpriteNode {
+    @discardableResult func texture(_ value: SKTexture) -> Next<SpriteModifiers.Texture<Node>> {
         self.modifier(mod: SpriteModifiers.Texture(body: value))
     }
     
-    @discardableResult func size(_ value: CGSize) -> Next<SpriteModifiers.Size> {
+    @discardableResult func size(_ value: CGSize) -> Next<SpriteModifiers.Size<Node>> {
         self.modifier(mod: SpriteModifiers.Size(body: value))
     }
     
-    @discardableResult func anchorPoint(_ value: CGPoint) -> Next<SpriteModifiers.AnchorPoint> {
+    @discardableResult func anchorPoint(_ value: CGPoint) -> Next<SpriteModifiers.AnchorPoint<Node>> {
         self.modifier(mod: SpriteModifiers.AnchorPoint(body: value))
     }
     
-    @discardableResult func centerRect(_ value: CGRect) -> Next<SpriteModifiers.CenterRect> {
+    @discardableResult func centerRect(_ value: CGRect) -> Next<SpriteModifiers.CenterRect<Node>> {
         self.modifier(mod: SpriteModifiers.CenterRect(body: value))
     }
     
-    @discardableResult func color(_ value: SKColor) -> Next<SpriteModifiers.Color> {
+    @discardableResult func color(_ value: SKColor) -> Next<SpriteModifiers.Color<Node>> {
         self.modifier(mod: SpriteModifiers.Color(body: value))
     }
     
-    @discardableResult func colorBlendFactor(_ value: CGFloat) -> Next<SpriteModifiers.ColorBlendFactor> {
+    @discardableResult func colorBlendFactor(_ value: CGFloat) -> Next<SpriteModifiers.ColorBlendFactor<Node>> {
         self.modifier(mod: SpriteModifiers.ColorBlendFactor(body: value))
     }
     
-    @discardableResult func blendMode(_ value: SKBlendMode) -> Next<SpriteModifiers.BlendMode> {
+    @discardableResult func blendMode(_ value: SKBlendMode) -> Next<SpriteModifiers.BlendMode<Node>> {
         self.modifier(mod: SpriteModifiers.BlendMode(body: value))
     }
     
-    @discardableResult func lightingBitMask(_ value: UInt32) -> Next<SpriteModifiers.LightingBitMask> {
+    @discardableResult func lightingBitMask(_ value: UInt32) -> Next<SpriteModifiers.LightingBitMask<Node>> {
         self.modifier(mod: SpriteModifiers.LightingBitMask(body: value))
     }
     
-    @discardableResult func shadowedBitMask(_ value: UInt32) -> Next<SpriteModifiers.ShadowedBitMask> {
+    @discardableResult func shadowedBitMask(_ value: UInt32) -> Next<SpriteModifiers.ShadowedBitMask<Node>> {
         self.modifier(mod: SpriteModifiers.ShadowedBitMask(body: value))
     }
     
-    @discardableResult func shadowCastBitMask(_ value: UInt32) -> Next<SpriteModifiers.ShadowCastBitMask> {
+    @discardableResult func shadowCastBitMask(_ value: UInt32) -> Next<SpriteModifiers.ShadowCastBitMask<Node>> {
         self.modifier(mod: SpriteModifiers.ShadowCastBitMask(body: value))
     }
     
-    @discardableResult func normalTexture(_ value: SKTexture?) -> Next<SpriteModifiers.NormalTexture> {
+    @discardableResult func normalTexture(_ value: SKTexture?) -> Next<SpriteModifiers.NormalTexture<Node>> {
         self.modifier(mod: SpriteModifiers.NormalTexture(body: value))
     }
     
-    @discardableResult func shader(_ value: SKShader?) -> Next<SpriteModifiers.Shader> {
+    @discardableResult func shader(_ value: SKShader?) -> Next<SpriteModifiers.Shader<Node>> {
         self.modifier(mod: SpriteModifiers.Shader(body: value))
     }
     
-    @discardableResult func setValue(_ value: SKAttributeValue, forAttribute key: String) -> Next<SpriteModifiers.SetValue> {
+    @discardableResult func setValue(_ value: SKAttributeValue, forAttribute key: String) -> Next<SpriteModifiers.SetValue<Node>> {
         self.modifier(mod: SpriteModifiers.SetValue(value: value, key: key))
     }
     
