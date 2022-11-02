915889f8969fadacd064958329912f31441fc524
rrbox <87851278+rrbox@users.noreply.github.com>
2022-11-02 13:21:04 +0900

    [add] SubclassTests 各ノードのサブクラスに対するテストを追加していく予定です.

---
+++ Tests/SKNodeBuilderTests/SubclassTests.swift
@@ -0,0 +1,28 @@
+//
+//  SubclassTests.swift
+//  
+//
+//  Created by rrbox on 2022/11/02.
+//
+
+import SpriteKit
+import XCTest
+
+class SpriteNode: SKSpriteNode {
+    
+}
+
+class LabelNode: SKLabelNode {
+    
+}
+
+class ShapeNode: SKShapeNode {
+    
+}
+
+final class SubclassTests: XCTestCase {
+    func testSubclass() {
+        
+    }
+
+}
