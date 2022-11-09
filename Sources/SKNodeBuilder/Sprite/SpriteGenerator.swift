7e94f664c716c55f14ac91e0e26849435c51136e
rrbox <87851278+rrbox@users.noreply.github.com>
2022-11-09 22:20:13 +0900

    [add] SpriteGenerator.swift を追加しました. SKSpriteNode のサブクラスを生成する関数を作成します.

---
+++ Sources/SKNodeBuilder/Sprite/SpriteGenerator.swift
@@ -0,0 +1,16 @@
+//
+//  SpriteGenerator.swift
+//  
+//
+//  Created by rrbox on 2022/11/09.
+//
+
+import SpriteKit
+
+public enum SpriteGenerators {
+    
+}
+
+public extension Generators where Node: SKSpriteNode {
+    
+}
