21151387e3044164bdabffa2605545f607d79dd6
rrbox <87851278+rrbox@users.noreply.github.com>
2022-11-10 00:01:03 +0900

    [add] Blank Generator, Processor が設定されていない Builder に代替として配置するデータです.

--- Sources/SKNodeBuilder/Foundation/Builder/BuilderSetup.swift
+++ Sources/SKNodeBuilder/Foundation/Builder/BuilderSetup.swift
@@ -5,4 +5,12 @@
 //  Created by rrbox on 2022/11/09.
 //
 
-import Foundation
+import SpriteKit
+
+public struct Blank<Node: SKNode>: GeneratorProtocol, ProcessorProtocol {
+    public typealias Mod = Empty<Node>
+    public var modData: Empty<Node>
+    
+    public func mod(node: Node) {}
+    
+}
