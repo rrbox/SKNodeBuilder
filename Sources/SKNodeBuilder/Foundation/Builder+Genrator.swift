5c5d4e75d5f357beb34b297ef1b20c008564e29a
rrbox <87851278+rrbox@users.noreply.github.com>
2022-11-09 23:37:11 +0900

    [move] `node()` の実装を Builder+Generator ファイルに移動しました.

--- Sources/SKNodeBuilder/Foundation/Builder+Genrator.swift
+++ Sources/SKNodeBuilder/Foundation/Builder+Genrator.swift
@@ -5,4 +5,30 @@
 //  Created by rrbox on 2022/11/09.
 //
 
-import Foundation
+public extension Builder where Generator: OptionalNodeGenerator {
+    func node() -> Generator.Node? {
+        guard let result = self.generator.node() else {
+            return nil
+        }
+        return self.processor.process(node: result)
+    }
+    
+}
+
+public extension Builder where Generator: ThrowableNodeGenerator {
+    func node() throws -> Generator.Node {
+        return self.processor.process(node: try self.generator.node())
+    }
+    
+}
+
+public extension Builder where Generator: DefaultNodeGenerator {
+    /// 定義されたビルダーからノードを生成します.
+    ///
+    /// Node の型によらず, ノードの生成は `Node()` から開始されます.
+    func node() -> Generator.Node {
+        return self.processor.process(node: self.generator.node())
+    }
+    
+}
+
--- Sources/SKNodeBuilder/Foundation/Builder.swift
+++ Sources/SKNodeBuilder/Foundation/Builder.swift
@@ -19,30 +19,3 @@ public struct Builder<Generator: GeneratorProtocol, T: ProcessorProtocol> where 
     }
     
 }
-
-public extension Builder where Generator: OptionalNodeGenerator {
-    func node() -> Generator.Node? {
-        guard let result = self.generator.node() else {
-            return nil
-        }
-        return self.processor.process(node: result)
-    }
-    
-}
-
-public extension Builder where Generator: ThrowableNodeGenerator {
-    func node() throws -> Generator.Node {
-        return self.processor.process(node: try self.generator.node())
-    }
-    
-}
-
-public extension Builder where Generator: DefaultNodeGenerator {
-    /// 定義されたビルダーからノードを生成します.
-    ///
-    /// Node の型によらず, ノードの生成は `Node()` から開始されます.
-    func node() -> Generator.Node {
-        return self.processor.process(node: self.generator.node())
-    }
-    
-}
